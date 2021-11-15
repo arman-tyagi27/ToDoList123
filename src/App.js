c9b41aeb77b1ab2937ae94c6194db20884a60e1c
Arman Tyagi <Arman.Tyagi@bhavnacorp.com>
2021-11-15 14:32:16 +0530

    first progrm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header';
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
 
 function App() {
+  let todos = [
+    {
+      Sno: 1,
+      title: "Arman Tyagi",
+      desc:"This is my name"
+    },
+    {
+      Sno: 2,
+      title: "Training",
+      desc:"I'm doing the software Engineer training "
+    },
+    {
+      Sno: 3,
+      title: "Web Developer",
+      desc:"I'm full stack developer"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title='My ToDo List '/>
+    <Todos todos={todos}/>
+    <Footer/>
+    </>   
   );
 }
 
